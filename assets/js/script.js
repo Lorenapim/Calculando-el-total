cc5ef97c79a48117838f045936d6090ca17d7853
Lorena <lorenapim@gmail.com>
2023-01-02 21:02:49 -0300

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var precio = 400000;
+
+// Input
+var cantidadInput = document.querySelector("input[type='number']");
+var colorInput = document.querySelector("input[type='text']");
+
+// Totales
+var precioFinal = document.querySelector(".valor-total");
+var cantidadFinal = document.querySelector(".cantidad-total");
+var colorFinal = document.querySelector(".color div");
+
+// Funciones
+function calcularTotal() {
+    var cantidad = cantidadInput.value;
+    var color = colorInput.value;
+
+    precioFinal.innerHTML = (precio * cantidad).
+    toLocaleString();
+    cantidadFinal.innerHTML = cantidad;
+    colorFinal.style.backgroundColor = color;
+}
